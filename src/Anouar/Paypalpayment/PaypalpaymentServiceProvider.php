bf52a4b33f9452f74943e6b9239befef7a32a70c
unknown <dte@gmain.com>
2013-07-21 21:07:31 +0100

    First commit

---
+++ src/Anouar/Paypalpayment/PaypalpaymentServiceProvider.php
@@ -0,0 +1,48 @@
+<?php namespace Anouar\Paypalpayment;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaypalpaymentServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('anouar/paypalpayment');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+		$this->app['paypalpayment'] = $this->app->share(function($app)
+        {
+            return new PaypalPayment;
+        });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('paypalpayment');
+	}
+
+}
