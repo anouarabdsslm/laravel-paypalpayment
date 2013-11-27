2dee44465a87f7db240f32bc3127c1006d4fadc7
Graham Campbell <graham@mineuk.com>
2013-11-27 21:51:16 +0000

    Fixed formatting

--- src/Anouar/Paypalpayment/PaypalPayment.php
+++ src/Anouar/Paypalpayment/PaypalPayment.php
@@ -28,173 +28,186 @@ use PayPal\Rest\ApiContext;
 use PayPal\Auth\OAuthTokenCredential;
 
 class PaypalPayment{
-	
+  
 
-	//Class functions
+    // Class functions
 
-	public static function Address(){
-   	 	return new Address();
-  	}
+    public static function Address()
+    {
+        return new Address();
+    }
 
-  	public static function Amount(){
-  		return new Amount();
-  	}
+    public static function Amount()
+    {
+        return new Amount();
+    }
 
-  	public static function AmountDetails(){
-  		return new AmountDetails();
-  	}
+    public static function AmountDetails()
+    {
+        return new AmountDetails();
+    }
 
-  	public static function Authorization(){
-  		return new Authorization();
-  	}
+    public static function Authorization()
+    {
+        return new Authorization();
+    }
 
-  	public static function Capture(){
-  		return new Capture();
-  	}
+    public static function Capture()
+    {
+        return new Capture();
+    }
 
-  	public static function CreditCard(){
+    public static function CreditCard()
+    {
+        return new CreditCard();
+    }
 
-  		return new CreditCard();
-  	}
+    public static function CreditCardToken()
+    {
+        return new CreditCardToken();
+    }
 
-  	public static function CreditCardToken(){
+    public static function FundingInstrument()
+    {
+        return new FundingInstrument();
+    }
 
-  		return new CreditCardToken();
-  	}
+    public static function Item()
+    {
+        return new Item();
+    }
 
-  	public static function FundingInstrument(){
+    public static function ItemList()
+    {
+        return new ItemList();
+    }
 
-  		return new FundingInstrument();
-  	}
+    public static function Link()
+    {
+        return new Link();
+    }
 
-  	public static function Item(){
+    public static function Payee()
+    {
+        return new Payee();
+    }
 
-  		return new Item();
-  	}
+    public static function Payer()
+    {
+        return new Payer();
+    }
 
-  	public static function ItemList(){
+    public static function PayerInfo()
+    {
+        return new PayerInfo();
+    }
 
-  		return new ItemList();
-  	}
+    public static function Payment()
+    {
+        return new Payment();
+    }
 
-  	public static function Link(){
+    public static function PaymentExecution()
+    {
+        return new PaymentExecution();
+    }
 
-  		return new Link();
-  	}
+    public static function PaymentHistory()
+    {
+        return new PaymentHistory();
+    }
 
-  	public static function Payee(){
+    public static function RedirectUrls()
+    {
+        return new RedirectUrls();
+    }
 
-  		return new Payee();
-  	}
+    public static function Refund()
+    {
+        return new Refund();
+    }
 
-  	public static function Payer(){
-  		return new Payer();
-  	}
+    public static function Resource()
+    {
+        return new Resource();
+    }
 
-  	public static function PayerInfo(){
-  		return new PayerInfo();
-  	}
+    public static function Sale()
+    {
+        return new Sale();
+    }
 
-  	public static function Payment(){
-  		return new Payment();
-  	}
+    public static function ShippingAddress()
+    {
+        return new ShippingAddress();
+    }
 
-  	public static function PaymentExecution(){
-  		return new PaymentExecution();
-  	}
+    public static function SubTransaction()
+    {
+        return new SubTransaction();
+    }
 
-  	public static function PaymentHistory(){
-  		return new PaymentHistory();
-  	}
-
-  	public static function RedirectUrls(){
-  		return new RedirectUrls();
-  	}
-
-  	public static function Refund(){
-  		return new Refund();
-  	}
-
-  	public static function Resource(){
-  		return new Resource();
-  	}
-
-  	public static function Sale(){
-  		return new Sale();
-  	}
-
-  	public static function ShippingAddress(){
-  		return new ShippingAddress();
-  	}
-
-  	public static function SubTransaction(){
-  		return new SubTransaction();
-  	}
-
-  	public static function Transaction(){
-  		return new Transaction();
-  	}
-
-
-
-  	public static function ApiContext($credential, $requestId=null){
-  		return new ApiContext($credential, $requestId);
-  	}
+    public static function Transaction()
+    {
+        return new Transaction();
+    }
 
 
-  	//Utilities fucntions
-	public static function OAuthTokenCredential($ClientId=null,$ClientSecret=null) {
+    public static function ApiContext($credential, $requestId=null)
+    {
+        return new ApiContext($credential, $requestId);
+    }
 
-		define("PP_CONFIG_PATH", __DIR__);
 
-		if(isset($ClientId) && isset($ClientSecret)){
+    // Utilities functions
 
-			return new OAuthTokenCredential($ClientId,$ClientSecret);
+    public static function OAuthTokenCredential($ClientId=null,$ClientSecret=null)
+    {
+        define("PP_CONFIG_PATH", __DIR__);
 
-		}
+        if(isset($ClientId) && isset($ClientSecret)) {
+          return new OAuthTokenCredential($ClientId,$ClientSecret);
+        }
 
-		$configManager 	= \PPConfigManager::getInstance();
-			// $cred is used by samples that include this bootstrap file
-			// This piece of code simply demonstrates how you can
-			// dynamically pass in a client id/secret instead of using
-			// the config file. If you do not need a way to pass
-			// in credentials dynamically, you can skip the
-			// <Resource>::setCredential($cred) calls that
-			// you see in the samples.
-	   	 	
-	   	 $cred =new OAuthTokenCredential(
-					$configManager->get('acct1.ClientId'),
-					$configManager->get('acct1.ClientSecret'));
-	   	 return $cred;
-		
-	}
+        $configManager  = \PPConfigManager::getInstance();
+
+        // $cred is used by samples that include this bootstrap file
+        // This piece of code simply demonstrates how you can
+        // dynamically pass in a client id/secret instead of using
+        // the config file. If you do not need a way to pass
+        // in credentials dynamically, you can skip the
+        // <Resource>::setCredential($cred) calls that
+        // you see in the samples.
+
+        $cred =new OAuthTokenCredential(
+            $configManager->get('acct1.ClientId'),
+            $configManager->get('acct1.ClientSecret'));
+
+        return $cred;
+    }
  
+    // utility functions that returns base url for
+    public static function getBaseUrl()
+    {
+        return URL::to('/');
+    }
 
-	/**
-	 * ### getBaseUrl function
-	 * // utility functions that returns base url for
-	 * // determining return/cancel urls
-	 * @return string
-	 */
-	public static function getBaseUrl() {
+    // grape payment details using the paymentId
+    public static function get($paymentId, $apiContext = null)
+    {
+        if (isset($apiContext)) {
+            return Payment::get($paymentId, $apiContext);
+        }
+        return Payment::get($paymentId);
+    }
 
-		return URL::to('/');
-	}
-
-	//grape payment details using the paymentId
-	public static function get($paymentId, $apiContext = null){
-     if (isset($apiContext)) {
-       return Payment::get($paymentId, $apiContext);
-     }
-  		return Payment::get($paymentId);
-  }
-
-  ////grape all payment details
-  public static function all($param,$apiContext = null){
-      if (isset($apiContext)) {
-       return Payment::all($param, $apiContext);
-     }
-      return Payment::all($param);
-  }
+    // grape all payment details
+    public static function all($param,$apiContext = null)
+    {
+        if (isset($apiContext)) {
+            return Payment::all($param, $apiContext);
+        }
+        return Payment::all($param);
+    }
 
 }
